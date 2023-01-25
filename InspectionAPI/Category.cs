47306869ef20f9e96911506d07c68b4e4595b138
Tanvir1423 <tanvir6561@gmail.com>
2023-01-25 12:23:18 +0600

    Add project files.

---
+++ InspectionAPI/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionAPI
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+
+        public string CategoryName { get; set; } = string.Empty;
+        [StringLength(200)]
+        public string CategoryTypes { get; set; } = string.Empty;
+    }
+}
---
+++ InspectionAPI/Migrations/20220419054031_cat.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InspectionAPI.Migrations
+{
+    public partial class cat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    CategoryTypes = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
